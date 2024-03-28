1c9de24b50717b472c28059d355159027cda5543
huglecrack <Hleplingard04@gmail.com>
2024-03-28 11:59:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,278 @@
+<template>
+  <Modal/>
+
+  <header>
+    <ul>
+      <li class="home"><a href="#"><img src="@/assets/images/accueil.png" alt="logoHome"></a></li>
+      <li><a href="@/lien_page/404.html"><img src="@/assets/images/cv.png" alt="logoCV"></a></li>
+      <li><a href="@/lien_page/404.html"><img src="@/assets/images/formulaire-de-contact.png" alt="logoFormulaire"></a></li>
+    </ul>
+  </header>
+
+  <main>
+    <section>
+
+      <h1>HUGO Leplingard</h1>
+      <h3>porte-folio</h3>
+      <p>Étudiant en développement Front-end au CEF, je m'appelle Hugo, je fais 1m84.</p>
+
+    </section>
+
+    <article>
+
+      <figure>
+        <h2>Curiculum Vitae</h2>
+
+        <div class="container">
+          <img src="@/assets/images/CVfond.png" alt="CVfond" class="fond-image">
+
+          <div class="middle">
+            <img src="@/assets/images/cv.png" alt="logoCV">
+          </div>
+        </div>
+      </figure>
+
+      <figure>
+        <h2>Formulaire</h2>
+
+        <div class="container">
+          <img src="@/assets/images/formulaireFond.png" alt="formulaireFond"  class="fond-image">
+
+          <div class="middle">
+            <img src="@/assets/images/formulaire-de-contact.png" alt="logoFormulaire">
+          </div>
+        </div>
+      </figure>
+
+    </article>
+  </main>
+
+    <aside>
+    <form class="containerEnvoie" action="#">
+      <input type="text" id="Name" placeholder="Nom/Prénom" required><br>
+      <input type="text" id="Subject" placeholder="Objet" required><br>
+      <textarea type="text" id="Message" placeholder="Votre message" autocomplete="off" rows="10" cols="10" required></textarea><br>
+
+      <button type="submit">Envoyer</button>
+    </form>
+  </aside>
+
+  <footer>
+
+    <div>
+        <a href="https://fr.linkedin.com/in/hugo-leplingard-924793262"><img src="@/assets/images/linkedin.png" alt="linkedin"></a>
+        <a href="@/lien_page/twi.html"><img src="@/assets/images/twitter.png" alt="twitter"></a>
+        <a href="https://github.com/Huglecrack/CV_en__ligne_SFLL"><img src="@/assets/images/github.png" alt="github"></a>
+    </div>
+
+    <p>Dernière mise à jour le 20 mars 2024</p>
+
+  </footer>
+</template>
+
+
+
+<script setup>
+  import Modal from '@/components/Modal.vue';
+
+  function EnvoieEmail(e) {
+    e.preventdefault();
+
+    const Name = document.getElementById('Name').value;
+    const Subject = document.getElementById('Subject').value;
+    const  Message = document.getElementById('Message').value;
+
+    Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "hleplingard04@gmail.com",
+      Password : "C318A590E2D14B0C0EBBD4B8BF3AF5F52EC2",
+      To : 'hleplingard04@gmail.com',
+      From : Name,
+      Subject : Subject,
+      Body : Message
+    }).then(
+      message => alert(message)
+    );
+    return alert('message bien envoyé');
+  }
+</script>
+
+
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400&family=Raleway:wght@400&display=swap');
+
+  header {
+    width: 100%;
+    top: 0;
+    left: 0;
+    background-color:#5c5c5c;
+    position: fixed;
+    z-index: 1;
+  }
+
+  header ul {
+    display: flex;
+    height: 100%;
+    width: 100%;
+    float: left;
+    padding: 0;
+    margin: 0;
+    transition: all 0.4s ease;
+      text-align: center;
+      justify-content: space-evenly;
+      flex-direction: row;
+  }
+
+  header ul li{
+      width: 33.33%;
+      padding: 20px;
+      list-style-type: none;
+  }
+
+  header ul li img {
+      width: 50px;
+      height: 50px;
+  }
+
+  header ul .home {
+      background-color: #4fffdc;
+  }
+
+  header ul li:hover {
+      background-color: #1d1d1d;
+  }
+
+  header a.home img:hover {
+      background-color: #1d1d1d;
+  }
+
+  main {
+      margin-top: 150px;
+  } 
+
+  main section {
+      margin-left: 40px;
+  }
+
+  main article {
+      display: flex;
+      justify-content: center;
+  }
+
+  main article h2 {
+      margin: 30px 160px;
+  }
+
+  .container {
+      width: 70%;
+      height: 90%;
+      position: relative;
+      background-color: #1d1d1d;
+  }
+    
+  .fond-image {
+      opacity: 1;
+      display: block;
+      width: 100%;
+      height: auto;
+      transition: .5s ease;
+      backface-visibility: hidden;
+  }
+
+  .container:hover .fond-image {
+      opacity: 0.3;
+  }
+
+  .middle {
+      transition: .5s ease;
+      opacity: 0;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+  }
+
+  .container:hover .middle {
+      opacity: 1;
+  }
+
+  .middle img {
+      width: 90px;
+      height: 90px;
+  }
+
+  .middle:hover {
+      cursor: zoom-in;
+  }
+
+  .container:active .fond-image {
+      opacity: 1;
+      z-index: 1;
+      transform:scale(1.5);
+  }
+
+  .container:active .middle {
+      opacity: 0;
+  }
+
+  aside {
+      display: flex;
+      justify-content: center;
+  }
+
+  aside form {
+      margin-top: 130px;
+      background-color: #5c5c5c;
+      padding: 20px;
+      border-radius: 8px;
+      box-shadow: 0 0 10px #5c5c5c;
+  }
+
+  aside form input, textarea {
+      display: block;
+      width: 500px;
+      padding: 8px;
+      margin-bottom: 16px;
+      box-sizing: border-box;
+      border: solid 2px #5c5c5c;
+      border-radius: 1%;
+  }
+
+  aside form button {
+      background-color: #4fffdc;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+  }
+
+  footer {
+      margin-top: 30px;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+  }
+
+  footer div {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+  }
+  footer div a img {
+      display: flex;
+      margin: 15px 30px 5px;
+      height: 70px;
+      width: 70px;
+  }
+
+  footer div a img:hover {
+      cursor: pointer;
+      opacity: 75%;
+  }
+
+  footer p {
+      font-size: 15px;
+  }
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,61 @@
+
+<template>
+    <div class="Modal">
+        <div class="fermerModalDiv">
+            <button class="envoieModal"><img src="../images/fermer.png"></button>
+        </div>
+        <h3>Porte-Folio d'HUGO Leplingard</h3>
+        <p>crée le Mercredi 6 mars 2024</p>
+        <p>Ce site est fait de 48.1% d'HTML, 42.7% de CSS et 9.2% de JavaScript</p>
+        <a href="../pages/index.html">Cliquez our voir le site</a>
+        <a href="https://github.com/Huglecrack/Portfolio-Project.git">Liens vers le Repository Github</a>
+    </div>
+</template>
+
+
+
+<script setup>
+
+</script>
+
+
+
+<style scoped>
+.modal {
+    width: 100%;
+    height: 40%;
+    display: flex;
+    z-index: 1;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-top:solid 4px #006652;
+    flex-direction: column;
+    align-items: center;
+    background-color: #3d3d3d;
+}
+
+.modal a {
+    text-decoration: none;
+    color: #00cba2;
+}
+
+.fermerModalDiv {
+    width: 100%;
+    margin-top: 15px;
+    display: flex;
+    justify-content: end;
+}
+
+.envoieModal,.envoieModal img {
+    display: flex;
+    width: 30px;
+    height: 30px;
+    margin-right: 15px;
+    border-radius: 15%;
+    background-color: #3d3d3d;
+    justify-content: center;
+    align-items: center;
+}
+</style>
