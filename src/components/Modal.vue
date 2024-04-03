dc1da1f9089b76d68c376730d10376604b71a5c0
huglecrack <Hleplingard04@gmail.com>
2024-04-03 10:08:04 +0200

    date

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -6,7 +6,7 @@
         <div class="modal">
             <img src="../assets/images/fermer.png" v-on:click="toggleModal">
             <h3>Porte-Folio d'HUGO Leplingard</h3>
-            <p>crée le Mercredi 6 mars 2024</p>
+            <p>ernière mise à jour le 02 avril 2024</p>
             <p>Ce site est fait de 88.9% de Vue, 4.8% de JavaScript, 4.5% d'HTML et de 1.8% de CSS</p>
             <a href="http://localhost:5175/" target="_blank">Cliquez pour voir le site</a><br>
             <a href="http://github.com/Huglecrack/Projet_-val-4_vue02.git" target="_blank" rel="noopener noreferrer">Liens vers le Repository Github</a>
